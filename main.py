ac71f6845b961fdf443e3d8b78163057bd761f4e
William Blanco <wblanco@sandiego.edu>
2026-07-15 19:02:31 -0700

    Create database tables on app start up

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
+from database import engine, Base
+from models import User
 
 app = FastAPI()
+
+Base.metadata.create_all(bind=engine)
 
 @app.get("/")
 def read_root():
